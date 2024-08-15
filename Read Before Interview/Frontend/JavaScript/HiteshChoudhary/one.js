c7d91464ebe83a729f6500a34f3df176da34466b
Shivangi Kumari <12shivangikumari@gmail.com>
2024-08-15 13:39:22 +0530

    updated frontend

---
+++ Read Before Interview/Frontend/JavaScript/HiteshChoudhary/one.js
@@ -0,0 +1,38 @@
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// // -----------------------------------------------------------
+
+// let score = "33abc"
+// console.log(typeof score); // string
+
+// let value = Number(score);
+// console.log(typeof value); // number
+// console.log(value); // NaN (not a number)
+// // convert hogya number mai but print krate tym NaN show ho rha
+
+// // -----------------------------------------------------------
+
+// console.log("1" + 1);
+// console.log("1" + 1 + 1);
+
+// console.log(1 + "1" + 1 + 1);
+// console.log(1 + 1 + "1" + 1 + 1);
+// console.log(1 + 1 + 1 + "1" + 1);
+// console.log(1 + 1 + 1 + 1 + "1");
+
+// // -----------------------------------------------------------
+
+// console.log(null == 0); // this is not comparison
+// console.log(null > 0);
+// console.log(null >= 0); // comparisons convert null to a number, treating it as 0
+
+// // -----------------------------------------------------------
+
+// console.log("2" > 1); // true
+// console.log("1" >= 1); // true
+
+// // -----------------------------------------------------------
+
+
+
