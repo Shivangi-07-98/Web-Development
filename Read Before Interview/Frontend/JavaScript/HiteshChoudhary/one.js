99426c9b304eaf9840b1c8a05669d133fe0c3b88
Shivangi Kumari <12shivangikumari@gmail.com>
2024-08-17 20:38:31 +0530

    updated HiteshChoudhary

--- Read Before Interview/Frontend/JavaScript/HiteshChoudhary/one.js
+++ Read Before Interview/Frontend/JavaScript/HiteshChoudhary/one.js
@@ -34,5 +34,62 @@
 
 // // -----------------------------------------------------------
 
+// // stack and heap concept 
+// // using primitive data type and you'll see its value doesn't change because it doesn't use reference but copies 
+
+// let golu = "intelligent";
+// let chunnu = golu;
+// chunnu = "dumb";
+// console.log(chunnu);
+// console.log(golu);
+
+// // value is changed now because value does not copy it uses reference
+// let golu1 = {
+//   quality: "intelligent"
+// }
+
+// let chunnu1 = golu1;
+
+// chunnu1.quality = "dumb";
+// console.log(chunnu1);
+// console.log(golu1);
+
+// // -----------------------------------------------------------
+
+// // spread operator
+// let start = [1, 2, 3]
+// let end = [4, 5, 6]
+// let all = [...start, ...end]
+// console.log(all);
+
+// // -----------------------------------------------------------
+
+
+// // Object literals
+
+// const Sym = Symbol("key1");
+// // let Sym;
+
+// const user = {
+//   name: "golu",
+//   "age": 18,
+//   "contact number": "abc",
+//   [Sym]: "mykey1"
+//   // contact number: "abc"
+// }
+
+// // console.log(user.name);
+// // console.log(user.age);
+// // console.log(user.contact number); // wrong
+
+// // console.log(user["name"]);
+// // console.log(user["age"]);
+// console.log(user["contact number"]);
+// console.log(user[Sym]);
+// console.log(user);
+
+
+// // -----------------------------------------------------------
+
 
 
